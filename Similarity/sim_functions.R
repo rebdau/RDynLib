ed66ae7cb8f199c7aab37c51a723e857d200a9ac
Ahlam Mentag <ahlammentag63@gmail.com>
2025-05-28 20:17:15 +0200

    Similarity calculation on matrix types

--- Similarity/sim_functions.R
+++ Similarity/sim_functions.R
@@ -19,77 +19,131 @@ remove_duplicates <- function(mz, intensity) {
   return(list(mz = unique_mz, intensity = max_intensity))
 }
 
-symmetric_dotproduct_combined <- function(x, y, xPrecursorMz, yPrecursorMz, n = 3, m = 0.6, ...) {
-  
-  mz1 <- as.numeric(unlist(x[, 1L]))
-  intensity1 <- as.numeric(unlist(x[, 2L]))
-  mz2 <- as.numeric(unlist(y[, 1L]))
-  intensity2 <- as.numeric(unlist(y[, 2L]))
-  
-  if (length(mz1) == 0 || length(mz2) == 0) 
+#' @description
+#'
+#' Function to calculate the symmetric dot product between two spectra. The
+#' function also:
+#'
+#' - *cleans* the spectra keeping only a single fragment peak for groups of
+#'   peaks with a m/z smaller 0.5
+#'
+#' @param x `numeric` 2-column `matrix` with the m/z and intensity values
+#'
+#' @param y `numeric` 2-column `matrix` with the m/z and intensity values
+#'
+#' @param xPrecursorMz `numeric(1)` with the precursor m/z of the spectrum `x`
+#'
+#' @param yPrecursorMz `numeric(1)` with the precursor m/z of the spectrum `y`
+#'
+#' @param n
+#'
+#' @param m
+#'
+#' @author Ahlam Mentag
+symmetric_dotproduct_combined <- function(x, y, n = 3, m = 0.6, ...) {
+  if (!nrow(x) || !nrow(y))
     return(list(common_peaks = 0, similarity_score = 0))
   
-  cleaned1 <- remove_duplicates(mz1, intensity1)
-  cleaned2 <- remove_duplicates(mz2, intensity2)
+  mz1 <- x[, 1L]
+  intensity1 <- x[, 2L]
+  mz2 <- y[, 1L]
+  intensity2 <- y[, 2L]
+  
+  intensity1 <- (intensity1^n) * (mz1^m)
+  intensity2 <- (intensity2^n) * (mz2^m)
+  
+  denom1 <- sum(intensity1^2)
+  denom2 <- sum(intensity2^2)
+  
+  common_mz <- intersect(mz1, mz2)
+  
+  nom <- 0
+  if (length(common_mz) > 0) {
+    idx1 <- match(common_mz, mz1)
+    idx2 <- match(common_mz, mz2)
+    nom <- sum(intensity1[idx1] * intensity2[idx2])
+  }
+  
+  similarity <- if (nom > 0) (nom^2) / (denom1 * denom2) else 0
+  
+  return(list(common_peaks = length(common_mz), similarity_score = similarity))
+}
+
+
+
+#' @description
+#'
+#' Matches/maps peaks between the two provided peak matrices.
+#'
+#' @return
+#'
+#' a list with numeric matrices containing only the matched peaks between
+#' the two input spectra.
+#'
+#' @author Ahlam Mentag
+dynlib_map <- function(x, y, xPrecursorMz, yPrecursorMz, ...) {
+  if (!nrow(x) || !nrow(y))
+    return(list(x = matrix(numeric(), ncol = 2, nrow = 0),
+                y = matrix(numeric(), ncol = 2, nrow = 0)))
+  
+  cleaned1 <- remove_duplicates(x[, 1L], x[, 2L])
+  cleaned2 <- remove_duplicates(y[, 1L], y[, 2L])
   
   mz1 <- cleaned1$mz
   intensity1 <- cleaned1$intensity
   mz2 <- cleaned2$mz
   intensity2 <- cleaned2$intensity
   
-  intensity1 <- (intensity1^n) * (mz1^m)
-  intensity2 <- (intensity2^n) * (mz2^m)
-  
-  Fd_denom1 <- sum(intensity1^2)
-  Fd_denom2 <- sum(intensity2^2)
-  
+  # Matching exact m/z
   common_mz <- intersect(mz1, mz2)
-  Fd_nom <- 0
-  remaining_idx1 <- rep(TRUE, length(mz1))
-  remaining_idx2 <- rep(TRUE, length(mz2))
+  matched1 <- cbind(mz1[mz1 %in% common_mz], intensity1[mz1 %in% common_mz])
+  matched2 <- cbind(mz2[mz2 %in% common_mz], intensity2[mz2 %in% common_mz])
   
-  if (length(common_mz) > 0) {
-    idx1 <- match(common_mz, mz1)
-    idx2 <- match(common_mz, mz2)
-    
-    valid_indices <- !is.na(idx1) & !is.na(idx2)
-    idx1 <- idx1[valid_indices]
-    idx2 <- idx2[valid_indices]
-    
-    if (length(idx1) > 0 && length(idx2) > 0) {
-      Fd_nom <- sum(intensity1[idx1] * intensity2[idx2])
-    }
-    
-    remaining_idx1[idx1] <- FALSE
-    remaining_idx2[idx2] <- FALSE
-  }
+  # Remaining peaks
+  remaining_idx1 <- !(mz1 %in% common_mz)
+  remaining_idx2 <- !(mz2 %in% common_mz)
   
-  # Calcul des neutral losses sur les pics restants
   nl1 <- neutral_loss(mz1[remaining_idx1], xPrecursorMz)
   nl2 <- neutral_loss(mz2[remaining_idx2], yPrecursorMz)
-  
   common_nl <- intersect(nl1, nl2)
   
-  if (length(common_nl) > 0) {
-    for (nl in common_nl) {
-      matches1 <- which(nl1 == nl)
-      matches2 <- which(nl2 == nl)
-      
-      if (length(matches1) > 0 && length(matches2) > 0) {
-        Fd_nom <- Fd_nom + sum(intensity1[remaining_idx1][matches1]) * 
-          sum(intensity2[remaining_idx2][matches2])
-      }
-    }
+  for (nl in common_nl) {
+    idx_nl1 <- which(nl1 == nl)
+    idx_nl2 <- which(nl2 == nl)
+    
+    matched_nl1 <- cbind(mz1[remaining_idx1][idx_nl1],
+                         intensity1[remaining_idx1][idx_nl1])
+    matched_nl2 <- cbind(mz2[remaining_idx2][idx_nl2],
+                         intensity2[remaining_idx2][idx_nl2])
+    
+    matched1 <- rbind(matched1, matched_nl1)
+    matched2 <- rbind(matched2, matched_nl2)
   }
   
-  FD <- if (Fd_nom > 0) (Fd_nom^2) / (Fd_denom1 * Fd_denom2) else 0
-  
-  total_common <- length(common_mz) + length(common_nl)
-  
-  return(FD)
+  return(list(x = matched1, y = matched2))
 }
 
 
+
+
+dynlib_fun <- function(x, y, ...) {
+  
+  peaks_x <- peaks(x)
+  peaks_y <- peaks(y)
+  
+
+  precursor_x <- precursorMz(x)
+  precursor_y <- precursorMz(y)
+  
+
+  matched <- dynlib_map(peaks_x, peaks_y, precursor_x, precursor_y)
+  
+ 
+  result <- symmetric_dotproduct_combined(matched$x, matched$y)
+  
+ 
+  return(result$similarity_score)
+}
 
 
 
@@ -154,14 +208,14 @@ count_common_peaks <- function(x, y, xPrecursorMz, yPrecursorMz, ...) {
 library(BiocParallel)
 library(Spectra)
 
-compare_with_metaboannotation <- function(st_sps, filtered_dy_sps, threshold = 0.8, ppm = 5, tolerance = 0.005) {
+final_sim <- function(st_sps, filtered_dy_sps, threshold = 0.8, ppm = 5, tolerance = 0.005) {
   library(MetaboAnnotation)
   
   param <- CompareSpectraParam(
     ppm = ppm,
     tolerance = tolerance,
     requirePrecursor = TRUE,
-    FUN = symmetric_dotproduct_combined,
+    FUN = dynlib_fun,
     threshold = threshold
   )
   
@@ -171,8 +225,8 @@ compare_with_metaboannotation <- function(st_sps, filtered_dy_sps, threshold = 0
     param = param
   )
   
-  df <- as.data.frame(matches)
-  df <- df[df$score >= threshold, ]
+  df <- matchedData(matches)
+  df <- df[!is.na(df$score) & df$score >= threshold, ]
   
   return(df)
 }
