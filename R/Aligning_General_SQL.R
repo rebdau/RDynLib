05fb4320cd633a1b4fc7f45d8fbd11d5b12ba5bb
Ahlam Mentag <ahlammentag63@gmail.com>
2025-11-26 14:31:35 +0100

    add documentation

--- R/Aligning_General_SQL.R
+++ R/Aligning_General_SQL.R
@@ -1,22 +1,32 @@
-#' @title
+#' @title Local alignment between FTMS and QTOF datasets
 #'
 #' @description
 #'
-#' The `Aligning_General_SQL()` function ...
+#' The `Aligning_General_SQL()` function performs local alignment between two
+#' LC-MS datasets stored in SQL databases (FTMS and QTOF).  
+#' The function:
+#' - extracts compound data from the both SQL databases,
+#' - performs local alignment using `LCalign()`,
+#' - adjusts `mass_measured` values when the two experiments have different 
+#'   polarities.
+#' @param FT_con `character(1)` A DBI database connection object to the FTMS 
+#'        SQL database.
 #'
-#' @param FT_con
+#' @param QTOF_con `character(1)` A DBI database connection object to the QTOF
+#'        SQL database.
 #'
-#' @param QTOF_con
+#' @param FT_expnr `numeric(1)` Experiment number (expid) to load from the FTMS
+#'         SQL database.
 #'
-#' @param FT_expnr
+#' @param QTOF_expnr `numeric(1)` Experiment number to load from the QTOF SQL
+#'         database.
 #'
-#' @param QTOF_expnr
+#' @param err `numeric(1)` is the m/z tolerance window used to decide which QTOF
+#'        peaks are potential isomeric matches for a given FT peak.
 #'
-#' @param err
+#' @param t.ini `numeric(1)`The number of neighboring peaks
 #'
-#' @param t.ini
-#'
-#' @return
+#' @return A data.frame representing the locally aligned features
 #'
 #' @author Ahlam Mentag
 #'
